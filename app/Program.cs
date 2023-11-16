0284ee4149e97a8ede498af911a0f5830097fcde
chatapazar <chatapazar@gmail.com>
2023-11-16 15:51:36 +0700

    update

--- app/Program.cs
+++ app/Program.cs
@@ -1,27 +1,62 @@
+using Prometheus;
+using Sample.Web;
+
+// This sample demonstrates how to integrate prometheus-net into a web app.
+// 
+// NuGet packages required:
+// * prometheus-net.AspNetCore
+// * prometheus-net.AspNetCore.HealthChecks (optional; for publishing health check results)
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+// Define an HTTP client that reports metrics about its usage, to be used by a sample background service.
+builder.Services.AddHttpClient(SampleService.HttpClientName);
+
+// Export metrics from all HTTP clients registered in services
+builder.Services.UseHttpClientMetrics();
+
+// A sample service that uses the above HTTP client.
+builder.Services.AddHostedService<SampleService>();
+
+builder.Services.AddHealthChecks()
+    // Define a sample health check that always signals healthy state.
+    .AddCheck<SampleHealthCheck>(nameof(SampleHealthCheck))
+    // Report health check results in the metrics output.
+    .ForwardToPrometheus();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseExceptionHandler("/Error");
 }
-
-app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
+// Capture metrics about all received HTTP requests.
+app.UseHttpMetrics();
+
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.UseEndpoints(endpoints =>
+{
+    // Enable the /metrics page to export Prometheus metrics.
+    // Open http://localhost:5099/metrics to see the metrics.
+    //
+    // Metrics published in this sample:
+    // * built-in process metrics giving basic information about the .NET runtime (enabled by default)
+    // * metrics from .NET Event Counters (enabled by default, updated every 10 seconds)
+    // * metrics from .NET Meters (enabled by default)
+    // * metrics about requests made by registered HTTP clients used in SampleService (configured above)
+    // * metrics about requests handled by the web app (configured above)
+    // * ASP.NET health check statuses (configured above)
+    // * custom business logic metrics published by the SampleService class
+    endpoints.MapMetrics();
+});
 
 app.Run();
